50569a858a23eb09fc49d3da5dcdf2b72c6cf65b
JC <90096971+AWeirdScratcher@users.noreply.github.com>
2023-11-16 22:29:56 +0800

    hello world

--- src/fc.hello-world.py
+++ src/fc.hello-world.py
@@ -1,13 +1,30 @@
-from preprompt import pf, Definitions
+from preprompt import of, pf, Definitions
 
-@pf
-def plugin():
-  """Run a plugin example."""
+# preprompt functions
+@pf()
+def hello(name: str):
+  """Say hello
+
+  Args:
+      name (str): Name
+  """
   return {
-    "response": "Hello, World!"
+    "response": "Hello, {}!".format(name)
+  }
+
+# openai function calling
+@of()
+def goodbye(name: str):
+  """Says goodbye
+
+  Args:
+      name (str): Name
+  """
+  return {
+    "response": "Bye, {}!".format(name)
   }
 
 __definitions__: Definitions = {
-  "preprompt-function": plugin(),
-  "openai": None
+  "preprompt-function": hello,
+  "openai": goodbye
 }
